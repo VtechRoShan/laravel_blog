af895426a5a7a7628e419b4263711aece9e7301e
soocrates <rooshan.vtech@gmail.com>
2024-05-14 02:24:20 +0545

    Added Workflow  update package.json

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,17 +1,17 @@
-@extends('layouts.master')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center align-items-center h-100 mt-5">
+    <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card mt-5">
-                <div class="card-header bg-dark text-white">{{ __('Login') }}</div>
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body mt-5">
+                <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="mb-3 row">
+                        <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
@@ -25,7 +25,7 @@
                             </div>
                         </div>
 
-                        <div class="mb-3 row">
+                        <div class="row mb-3">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
@@ -39,7 +39,7 @@
                             </div>
                         </div>
 
-                        <div class="mb-3 row">
+                        <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -51,7 +51,7 @@
                             </div>
                         </div>
 
-                        <div class="mb-0 row">
+                        <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
@@ -66,7 +66,6 @@
                         </div>
                     </form>
                 </div>
-                
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -46,3 +46,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
 
 
 // Hello Test 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
